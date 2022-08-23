ab79d30eb0272c609dbf13d92f4addeb0d145401
Vycheslav <general160473@yandex.ru>
2022-08-23 15:09:49 +0500

    add "homework 19"

--- project001/Program.cs
+++ project001/Program.cs
@@ -1,1 +1,23 @@
-﻿// 1,3,3,4
+﻿// Напишите программу, которая принимает на вход пятизначное 
+//число и проверяет, является ли оно палиндромом. Задача19
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber( string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Введенное число: {number} - палиндром");
+  }
+  else Console.WriteLine($"Введенное число: {number} - не палиндром");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
--- project003/Program.cs
+++ project003/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("11Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт 
+//таблицу кубов чисел от 1 до N. Задача 23
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+using static System.Console;
+Clear();
+WriteLine("Введите число N: ");
+int n = Convert.ToInt32(ReadLine());
+
+for(int i=1; i<=n; i++) 
+{
+    Write($"{Math.Pow(i,3)} ");
+  
+}
